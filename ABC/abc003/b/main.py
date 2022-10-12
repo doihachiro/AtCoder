40f74c0c8c527cb17564d308b8f54473dac067d4
doihachiro <raiton20@gmail.com>
2022-10-12 22:16:34 +0900

    Add abc003_b

--- ABC/abc003/b/main.py
+++ ABC/abc003/b/main.py
@@ -1,1 +1,12 @@
-A, B = map(int, input().split())
+S = input()
+T = input()
+X = "atcoder"
+
+for s, t in zip(S, T):
+    if (s == t) or (s == "@" and t in X) or (s in X and t == "@"):
+         continue
+    else:
+        print("You will lose")
+        exit()
+
+print("You can win")
