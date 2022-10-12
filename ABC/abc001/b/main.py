5c16feebde03673eb06ea51382ad5b51b04172f8
doihachiro <raiton20@gmail.com>
2022-10-12 22:16:34 +0900

    Add abc001_b

--- ABC/abc001/b/main.py
+++ ABC/abc001/b/main.py
@@ -1,1 +1,16 @@
-A, B = map(int, input().split())
+M = int(input())
+m = M / 1000
+
+if m < 0.1:
+    print("00")
+elif 0.1 <= m and m <= 5:
+    if (m * 10) < 10:
+        print("0" + str(int(m * 10)))
+    else:
+        print(int(m * 10))
+elif 6 <= m and m <= 30:
+    print(int(m + 50))
+elif 35 <= m and m <= 70:
+    print(int(74 + m / 5))
+elif 70 < m:
+    print(89)
