3835025047d558dde9b0d02556e821be66745578
doihachiro <raiton20@gmail.com>
2022-10-13 21:47:05 +0900

    Add abc010_b

--- ABC/abc010/b/main.py
+++ ABC/abc010/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
+c = 0
+
+for i in range(N):
+    while (A[i] % 2 == 0) or A[i] in [2, 5, 8]:
+        c += 1
+        A[i] -= 1 
+
+print(c)
