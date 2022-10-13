75731f35ee5b72bc8ffa27e6c6b1442a8d7ee920
doihachiro <raiton20@gmail.com>
2022-10-13 21:47:05 +0900

    Add abc008_b

--- ABC/abc008/b/main.py
+++ ABC/abc008/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+from collections import Counter
+
+N = int(input())
+X = []
+
+for _ in range(N):
+    X.append(input())
+
+A = Counter(X)
+print(A.most_common(1)[0][0])
