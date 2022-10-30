8b0e8ab03586846ba96e528234da9f217157c9d4
doihachiro <raiton20@gmail.com>
2022-10-30 13:25:06 +0900

    Add abc016_b

--- ABC/abc016/b/main.py
+++ ABC/abc016/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+A, B, C = map(int, input().split())
+
+X = A + B
+Y = A - B
+
+if C == X and C == Y:
+    print("?")
+elif C == X:
+    print("+")
+elif C == Y:
+    print("-")
+else:
+    print("!")
