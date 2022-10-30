9241849689bf25971465b5112a78efa57024ca36
doihachiro <raiton20@gmail.com>
2022-10-30 13:25:06 +0900

    Add abc015_b

--- ABC/abc015/b/main.py
+++ ABC/abc015/b/main.py
@@ -1,1 +1,12 @@
-A, B = map(int, input().split())
+from math import ceil, floor
+
+N = int(input())
+tmp = list(map(int, input().split()))
+A = 0
+
+for i in range(N):
+    if tmp[i] == 0:
+        N -= 1
+    A += tmp[i]
+
+print(ceil(A / N))
