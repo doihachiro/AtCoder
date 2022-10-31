f4d70ae0181cf0ba844c92c96ec87ba132b7ca46
doihachiro <raiton20@gmail.com>
2022-10-31 19:58:40 +0900

    Add abc017_b

--- ABC/abc017/b/main.py
+++ ABC/abc017/b/main.py
@@ -1,1 +1,7 @@
-A, B = map(int, input().split())
+X = input()
+A = X.replace("ch", "").replace("o", "").replace("k", "").replace("u", "")
+
+if len(A) == 0:
+    print("YES")
+else:
+    print("NO")
