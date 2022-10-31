f2cfac8da6be227ba1e3e87d05bbc40a913205da
doihachiro <raiton20@gmail.com>
2022-10-31 19:58:40 +0900

    Add abc024_b

--- ABC/abc024/b/main.py
+++ ABC/abc024/b/main.py
@@ -1,1 +1,14 @@
-A, B = map(int, input().split())
+N, T = map(int, input().split())
+count = 0
+X = []
+
+for i in range(N):
+    X.append(int(input()))
+
+for j in range(1, N):
+    if (X[j] - X[j-1]) >= T:
+        count += T
+    else:
+        count += (X[j] - X[j-1])
+
+print(count + T)
