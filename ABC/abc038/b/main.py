a9f196d22695d892c413702579bc3a697d113801
doihachiro <raiton20@gmail.com>
2022-11-04 01:07:05 +0900

    Add abc038_b

--- ABC/abc038/b/main.py
+++ ABC/abc038/b/main.py
@@ -1,1 +1,9 @@
-A, B = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+if A[0] == B[0] or A[0] == B[1]:
+    print("YES")
+elif A[1] == B[0] or A[1] == B[1]:
+    print("YES")
+else:
+    print("NO")
