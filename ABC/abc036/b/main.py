a00c09a5c22e24e5a02f2abb5de8a9a722b8f5dc
doihachiro <raiton20@gmail.com>
2022-11-04 00:51:07 +0900

    Add abc036_b

--- ABC/abc036/b/main.py
+++ ABC/abc036/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+N = int(input())
+S = []
+
+for _ in range(N):
+    S.append(input())
+
+for i in range(N):
+    ans = ""
+    for j in reversed(range(N)):
+        ans += S[j][i]
+    print(ans)
