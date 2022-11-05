3d78d03d3648e86f5862ca4d6be8849fc4d76466
doihachiro <raiton20@gmail.com>
2022-11-05 21:15:23 +0900

    Add abc051_b

--- ABC/abc051/b/main.py
+++ ABC/abc051/b/main.py
@@ -1,1 +1,8 @@
-A, B = map(int, input().split())
+K, S = map(int, input().split())
+count = 0
+
+for i in range(K + 1):
+    for j in range(K + 1):
+        if  0 <= S - (i + j) <= K:
+            count += 1
+print(count)
