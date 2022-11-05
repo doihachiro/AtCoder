10bbbd53aa29d7684aa30c802b59c0983d3cfc34
doihachiro <raiton20@gmail.com>
2022-11-05 18:22:14 +0900

    Add abc045_b

--- ABC/abc045/b/main.py
+++ ABC/abc045/b/main.py
@@ -1,1 +1,23 @@
-A, B = map(int, input().split())
+A = list(input())
+B = list(input())
+C = list(input())
+X = A.pop(0)
+
+while True:
+    if X == "a":
+        if len(A) == 0:
+            print("A")
+            break
+        X = A.pop(0)
+
+    if X == "b":
+        if len(B) == 0:
+            print("B")
+            break
+        X = B.pop(0)
+
+    if X == "c":
+        if len(C) == 0:
+            print("C")
+            break
+        X = C.pop(0)
