96fbf19bca4d1b9062029da6bae4cd849809e10f
doihachiro <raiton20@gmail.com>
2022-11-05 18:22:14 +0900

    Add abc043_b

--- ABC/abc043/b/main.py
+++ ABC/abc043/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+s = input()
+A = []
+
+for i in range(len(s)):
+    if s[i] == "0" or s[i] == "1":
+        A.append(s[i])
+    elif len(A) > 0 and s[i] == "B":
+        A.pop()
+
+print("".join(A))
