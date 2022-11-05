e120191ffad58655d894b96a25ab05a1075f2d44
doihachiro <raiton20@gmail.com>
2022-11-06 01:31:04 +0900

    Add abc056_b

--- ABC/abc056/b/main.py
+++ ABC/abc056/b/main.py
@@ -1,1 +1,8 @@
-A, B = map(int, input().split())
+w, a, b = map(int, input().split())
+
+if b - (a + w) > 0:
+    print(b - (a + w))
+elif a - (b + w) > 0:
+    print(a - (b + w))
+else:
+    print(0)
