4bf17ac70d5b3596f68b5810acb69285792f241b
doihachiro <raiton20@gmail.com>
2022-11-05 21:15:23 +0900

    Add abc052_b

--- ABC/abc052/b/main.py
+++ ABC/abc052/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+N = int(input())
+S = list(input())
+tmp = 0
+ans = 0
+
+for i in range(N):
+    if S[i] == "I":
+        tmp += 1
+    elif S[i] == "D":
+        tmp -= 1
+    ans = max(ans, tmp)
+
+print(ans)
