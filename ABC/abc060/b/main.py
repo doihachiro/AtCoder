2212490b05de2435256525fc430cd62a39fd55e7
doihachiro <raiton20@gmail.com>
2022-11-06 11:24:04 +0900

    Add abc060_b

--- ABC/abc060/b/main.py
+++ ABC/abc060/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+A, B, C = map(int, input().split())
+c = 1
+
+while (A * c) < 10 ** 5:
+    if (A * c) % B == C:
+        print("YES")
+        exit()
+    else:
+        c += 1
+
+print("NO")
