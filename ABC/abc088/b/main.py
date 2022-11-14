b6bb72402a76e94843a9264c49ab73c37507d323
doihachiro <raiton20@gmail.com>
2022-11-14 23:34:23 +0900

    Add abc088_b

--- ABC/abc088/b/main.py
+++ ABC/abc088/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+N = int(input())
+a = list(map(int, input().split()))
+a.sort(reverse=True)
+A = 0
+B = 0
+
+for i in range(0, N, 2):
+    A += a[i]
+
+for j in range(1, N, 2):
+    B += a[j]
+
+print(abs(A - B))
