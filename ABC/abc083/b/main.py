672a5f3fafd1f3166e45e9ddd05b3bef2949453d
doihachiro <raiton20@gmail.com>
2022-11-14 23:34:02 +0900

    Add abc083_b

--- ABC/abc083/b/main.py
+++ ABC/abc083/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+N, A, B = map(int, input().split())
+ans = []
+
+for i in range(1, N+1):
+    tmp = 0
+    for j in str(i):
+        tmp += int(j)
+    if A <= tmp and tmp <= B:
+        ans.append(i)
+
+print(sum(ans))
