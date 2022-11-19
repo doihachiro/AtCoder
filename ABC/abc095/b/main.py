8463f9534eb998a57dd51b3249be2cec83f7cb50
doihachiro <raiton20@gmail.com>
2022-11-20 02:12:50 +0900

    Add abc095_b

--- ABC/abc095/b/main.py
+++ ABC/abc095/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+N, X = map(int, input().split())
+min_m = 1000
+count = 0
+
+for i in range(N):
+    tmp = int(input())
+    X -= tmp
+    min_m = min(min_m, tmp)
+    count += 1
+
+print(count + (X // min_m))
