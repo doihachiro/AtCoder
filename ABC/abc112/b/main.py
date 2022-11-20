7705c84f0e71c8e1e9b9e9244383e92bbbab2da5
doihachiro <raiton20@gmail.com>
2022-11-21 00:42:06 +0900

    Add abc112_b

--- ABC/abc112/b/main.py
+++ ABC/abc112/b/main.py
@@ -1,1 +1,14 @@
-A, B = map(int, input().split())
+N, T = map(int, input().split())
+cost_ans = 1001
+time_ans = False
+
+for i in range(N):
+    cost_tmp, time_tmp = map(int, input().split())
+    if time_tmp <= T:
+        cost_ans = min(cost_ans, cost_tmp)
+        time_ans = True
+
+if time_ans == False:
+    print("TLE")
+else:
+    print(cost_ans)
