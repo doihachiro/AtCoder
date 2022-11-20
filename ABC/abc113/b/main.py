986f23cd52672d5d64116e71a032b304aaf5802b
doihachiro <raiton20@gmail.com>
2022-11-21 00:42:06 +0900

    Add abc113_b

--- ABC/abc113/b/main.py
+++ ABC/abc113/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+N = int(input())
+T, A = map(int, input().split())
+H = list(map(int, input().split()))
+tmp = 1000
+
+for i in range(N):
+    if abs(A - (T - H[i] * 0.006)) < tmp:
+        tmp = abs(A - (T - H[i] * 0.006))
+        ans = i + 1
+
+print(ans)
