3043de6e2395eaf97bf9a8a50751c9d1dc97336a
doihachiro <raiton20@gmail.com>
2022-11-20 15:31:17 +0900

    Add abc103_b

--- ABC/abc103/b/main.py
+++ ABC/abc103/b/main.py
@@ -1,1 +1,12 @@
-A, B = map(int, input().split())
+S = input()
+N = input()
+
+for i in range(len(S)):
+    tmp = S[-1] + S[:-1]
+    if tmp == N:
+        print("Yes")
+        exit()
+    else:
+        S = tmp
+
+print("No")
