110c0954f0150b128a15b1ace7216e3ab70f67a2
doihachiro <raiton20@gmail.com>
2022-11-21 00:42:06 +0900

    Add abc109_b

--- ABC/abc109/b/main.py
+++ ABC/abc109/b/main.py
@@ -1,1 +1,16 @@
-A, B = map(int, input().split())
+N = int(input())
+A = []
+
+for _ in range(N):
+    A.append(input())
+
+for i in range(1, N):
+    if A[i-1][-1] != A[i][0]:
+        print("No")
+        exit()
+
+if len(set(A)) != N:
+    print("No")
+    exit()
+
+print("Yes")
