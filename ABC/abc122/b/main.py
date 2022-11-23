5c1e5d6d76424b3abda41dd4d4a0a389dfb578a3
doihachiro <raiton20@gmail.com>
2022-11-23 20:28:31 +0900

    Add abc122_b

--- ABC/abc122/b/main.py
+++ ABC/abc122/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+S = input()
+count = 0
+ans = 0
+
+for i in S:
+    if i in ("A", "C", "G", "T"):
+        count += 1
+    else:
+        ans = max(ans, count)
+        count = 0
+
+ans = max(ans, count)
+print(ans)
