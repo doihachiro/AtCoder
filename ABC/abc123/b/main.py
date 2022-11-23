7a2736ebbea28f93fb00f3f0a35f697e6b7e5a0a
doihachiro <raiton20@gmail.com>
2022-11-23 20:28:31 +0900

    Add abc123_b

--- ABC/abc123/b/main.py
+++ ABC/abc123/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+X = []
+Y = [0]
+
+for _ in range(5):
+    tmp = int(input())
+    X.append(tmp)
+    if tmp % 10 != 0:
+        Y.append(10 - (tmp % 10))
+
+print(sum(X) + sum(Y) - max(Y))
