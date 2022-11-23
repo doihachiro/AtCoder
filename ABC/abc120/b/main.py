509b5fbafaf1b586deff7208a69373683af12de7
doihachiro <raiton20@gmail.com>
2022-11-23 20:28:31 +0900

    Add abc120_b

--- ABC/abc120/b/main.py
+++ ABC/abc120/b/main.py
@@ -1,1 +1,8 @@
-A, B = map(int, input().split())
+A, B, K = map(int, input().split())
+ans = []
+
+for i in range(1, 101):
+    if A % i == 0 and B % i == 0:
+        ans.append(i)
+
+print(ans[-K])
