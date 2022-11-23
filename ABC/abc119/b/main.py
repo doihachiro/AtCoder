af8e4401023bc35f0a6476d13205e966a4657563
doihachiro <raiton20@gmail.com>
2022-11-23 20:28:31 +0900

    Add abc119_b

--- ABC/abc119/b/main.py
+++ ABC/abc119/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N = int(input())
+ans = 0
+
+for i in range(N):
+    x, y = input().split()
+    if y == "BTC":
+        x = float(x) * 380000.0
+    ans += float(x)
+
+print(ans)
