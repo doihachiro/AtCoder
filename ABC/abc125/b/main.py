0759ca9ffb167151abf54816a4c16c9538a30abf
doihachiro <raiton20@gmail.com>
2022-11-25 02:44:19 +0900

    Add abc125_b

--- ABC/abc125/b/main.py
+++ ABC/abc125/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N = int(input())
+V = list(map(int, input().split()))
+C = list(map(int, input().split()))
+ans =0
+
+for i in range(N):
+    if V[i] > C[i]:
+        ans += V[i] - C[i]
+
+print(ans)
