dd693e6f2e7e0002b1176c13d2359ba9c8ebd598
doihachiro <raiton20@gmail.com>
2022-11-29 02:20:12 +0900

    Add abc136_b

--- ABC/abc136/b/main.py
+++ ABC/abc136/b/main.py
@@ -1,1 +1,14 @@
-A, B = map(int, input().split())
+N = int(input())
+
+if N < 10 ** 1:
+    print(N)
+elif N < 10 ** 2:
+    print(9)
+elif N < 10 ** 3:
+    print(N - 90)
+elif N < 10 ** 4:
+    print(909)
+elif N < 10 ** 5:
+    print(N - 9090)
+else:
+    print(90909)
