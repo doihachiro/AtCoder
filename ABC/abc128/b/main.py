e13e3b2b297706495a361475bd918c734d9f09a3
doihachiro <raiton20@gmail.com>
2022-11-29 02:19:24 +0900

    Add abc128_b

--- ABC/abc128/b/main.py
+++ ABC/abc128/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+N = int(input())
+ans = []
+
+for i in range(N):
+    S, P  = input().split()
+    ans.append([S, -int(P), i + 1])
+
+ans.sort()
+
+for j in ans:
+    print(j[2])
