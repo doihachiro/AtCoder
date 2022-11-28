aebb821bf4ef1aa0170a66d0ee3379224bf94e3b
doihachiro <raiton20@gmail.com>
2022-11-29 02:19:24 +0900

    Add abc132_b

--- ABC/abc132/b/main.py
+++ ABC/abc132/b/main.py
@@ -1,1 +1,9 @@
-A, B = map(int, input().split())
+n = int(input())
+p = list(map(int, input().split()))
+count = 0
+
+for i in range(2, n):
+    if (p[i-2] < p[i-1] < p[i]) or (p[i] < p[i-1] < p[i-2]):
+        count += 1
+
+print(count)
