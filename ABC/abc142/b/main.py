46de598c689f87f536e7b18720a4c638adbc38f4
doihachiro <raiton20@gmail.com>
2023-01-03 17:17:30 +0900

    Add abc142_b

--- ABC/abc142/b/main.py
+++ ABC/abc142/b/main.py
@@ -1,1 +1,9 @@
-A, B = map(int, input().split())
+N, K = map(int, input().split())
+h = list(map(int, input().split()))
+count = 0
+
+for i in range(N):
+    if K <= h[i]:
+        count += 1
+
+print(count)
