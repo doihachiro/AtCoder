9812a9d5b2b2d8cd5387ec83d191c3c485d85d60
doihachiro <raiton20@gmail.com>
2023-01-03 17:17:30 +0900

    Add abc144_b

--- ABC/abc144/b/main.py
+++ ABC/abc144/b/main.py
@@ -1,1 +1,9 @@
-A, B = map(int, input().split())
+N = int(input())
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        if i * j == N:
+            print("Yes")
+            exit()
+
+print("No")
