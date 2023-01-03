5e5b8676ad9db555b7f3bacff842e675956c6b36
doihachiro <raiton20@gmail.com>
2023-01-03 17:17:30 +0900

    Add abc141_b

--- ABC/abc141/b/main.py
+++ ABC/abc141/b/main.py
@@ -1,1 +1,8 @@
-A, B = map(int, input().split())
+S = input()
+
+for i in range(1, len(S)+1):
+    if ((i % 2 != 0) and (S[i-1] == "L")) or ((i % 2 == 0) and (S[i-1] == "R")):
+        print("No")
+        exit()
+
+print("Yes") 
