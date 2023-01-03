581e01b1cc772ca6283161a7e1859b95c8298d9c
doihachiro <raiton20@gmail.com>
2023-01-03 17:33:45 +0900

    Add abc146_b

--- ABC/abc146/b/main.py
+++ ABC/abc146/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+N = int(input())
+S = list(input())
+ans = []
+
+for i in range(len(S)):
+    tmp = ord(S[i]) + N
+    if 90 < tmp:
+        tmp -= 26
+    ans += chr(tmp)
+
+print("".join(ans))
