91118c6f371048deaed5feb117ae1331f5fa3d6b
doihachiro <raiton20@gmail.com>
2023-01-04 17:46:17 +0900

    Add abc151_b

--- ABC/abc151/b/main.py
+++ ABC/abc151/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N, K, M = map(int, input().split())
+A = list(map(int, input().split()))
+X = N * M
+
+if K < (X - sum(A)):
+    print(-1)
+elif X < sum(A):
+    print(0)
+else:
+    print(X - sum(A))
