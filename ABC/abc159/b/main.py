2f02869646400439dcd5e163c163f5637a1fb287
doihachiro <raiton20@gmail.com>
2023-01-05 22:48:16 +0900

    Add abc159_b

--- ABC/abc159/b/main.py
+++ ABC/abc159/b/main.py
@@ -1,1 +1,18 @@
-A, B = map(int, input().split())
+S = input()
+N = len(S)
+
+if S != S[N::-1]:
+    print("No")
+    exit()
+
+tmp = S[:(N-1)//2]
+if tmp != tmp[len(tmp)::-1]:
+    print("No")
+    exit()
+
+tmp = S[(N+2)//2:]
+if tmp != tmp[len(tmp)::-1]:
+    print("No")
+    exit()
+
+print("Yes")
