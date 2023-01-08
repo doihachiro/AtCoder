cb9b574b3b72f5733791eec13ff7c931b4da4d4b
doihachiro <raiton20@gmail.com>
2023-01-08 16:57:20 +0900

    Add abc164_b

--- ABC/abc164/b/main.py
+++ ABC/abc164/b/main.py
@@ -1,1 +1,12 @@
-A, B = map(int, input().split())
+A, B, C, D = map(int, input().split())
+
+while True:
+    C -= B
+    if C <= 0:
+        print("Yes")
+        exit()
+    
+    A -= D
+    if A <= 0:
+        print("No")
+        exit()
