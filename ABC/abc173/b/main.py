64db422354828dcc9e582ffb0aaba531acd1771a
doihachiro <raiton20@gmail.com>
2023-01-08 16:58:40 +0900

    Add abc173_b

--- ABC/abc173/b/main.py
+++ ABC/abc173/b/main.py
@@ -1,1 +1,21 @@
-A, B = map(int, input().split())
+N = int(input())
+AC  = 0
+WA  = 0
+TLE = 0
+RE  = 0
+
+for _ in range(N):
+    tmp = input()
+    if tmp == "AC":
+        AC += 1
+    elif tmp == "WA":
+        WA += 1
+    elif tmp == "TLE":
+        TLE += 1
+    elif tmp == "RE":
+        RE += 1
+
+print("AC x " + str(AC))
+print("WA x " + str(WA))
+print("TLE x " + str(TLE))
+print("RE x " + str(RE))
