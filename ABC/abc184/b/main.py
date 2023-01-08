8bd05f43df4a2c7e6352a1d5dfaaef14e78da578
doihachiro <raiton20@gmail.com>
2023-01-08 19:39:24 +0900

    Add abc184_b

--- ABC/abc184/b/main.py
+++ ABC/abc184/b/main.py
@@ -1,1 +1,12 @@
-A, B = map(int, input().split())
+N, X = map(int, input().split())
+S = input()
+
+for i in range(N):
+    if S[i] == "o":
+        X += 1
+    else:
+        X -= 1
+        if X < 0:
+            X = 0
+        
+print(X)
