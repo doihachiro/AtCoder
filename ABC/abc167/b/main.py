e8403b5cfc5629c5cac8abd4d6ecab9d081c2aa0
doihachiro <raiton20@gmail.com>
2023-01-08 16:57:21 +0900

    Add abc167_b

--- ABC/abc167/b/main.py
+++ ABC/abc167/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+A, B, C, K = map(int, input().split())
+ans = 0
+
+if K <= A:
+    ans = K
+elif K <= A + B:
+    ans = A
+else:
+    ans = A - (K - A - B)
+
+print(ans)
