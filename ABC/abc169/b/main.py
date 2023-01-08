f19f1f54bfe25d529af91ede00dd5807f741f9c3
doihachiro <raiton20@gmail.com>
2023-01-08 16:57:59 +0900

    Add abc169_b

--- ABC/abc169/b/main.py
+++ ABC/abc169/b/main.py
@@ -1,1 +1,15 @@
-A, B = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
+ans = 1
+
+if 0 in A:
+    print(0)
+    exit()
+
+for i in A:
+    ans *= i
+    if 10 ** 18 < ans:
+        print(-1)
+        exit()
+
+print(ans)
