89a997eb171bfec66075cd7081bbc2d2b5b166cb
doihachiro <raiton20@gmail.com>
2023-01-08 16:58:40 +0900

    Add abc174_b

--- ABC/abc174/b/main.py
+++ ABC/abc174/b/main.py
@@ -1,1 +1,9 @@
-A, B = map(int, input().split())
+N, D = map(int, input().split())
+count = 0
+
+for _ in range(N):
+    X, Y = map(int, input().split())
+    if (((X ** 2) + (Y ** 2)) ** 0.5) <= D:
+        count += 1
+
+print(count)
