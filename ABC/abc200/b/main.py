ee6fa2400623417dfca4357c643608532630b725
doihachiro <raiton20@gmail.com>
2023-01-10 00:37:51 +0900

    Add abc200_b

--- ABC/abc200/b/main.py
+++ ABC/abc200/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N, K = map(int, input().split())
+ans = N
+
+for i in range(K):
+    if ans % 200 == 0:
+        ans //= 200
+    else:
+        ans = int(str(ans) + "200")
+
+print(ans)
