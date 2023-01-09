cc1a4cd556f281c3db02ad4afc6827929d6c4e8a
doihachiro <raiton20@gmail.com>
2023-01-09 14:20:07 +0900

    Add abc190_b

--- ABC/abc190/b/main.py
+++ ABC/abc190/b/main.py
@@ -1,1 +1,9 @@
-A, B = map(int, input().split())
+N, S, D = map(int, input().split())
+
+for i in range(N):
+    X, Y = map(int, input().split())
+    if X < S and D < Y:
+        print("Yes")
+        exit()
+
+print("No")
