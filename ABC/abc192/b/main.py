fa8aa215d8d5f3ff549b266f66804c2608909490
doihachiro <raiton20@gmail.com>
2023-01-09 14:20:07 +0900

    Add abc192_b

--- ABC/abc192/b/main.py
+++ ABC/abc192/b/main.py
@@ -1,1 +1,8 @@
-A, B = map(int, input().split())
+S = input()
+
+for i in range(1, len(S)+1):
+    if (i % 2 != 0 and S[i-1].isupper()) or (i % 2 == 0 and S[i-1].islower()):
+        print("No")
+        exit()
+
+print("Yes")
