e0fbc7be82302ca79bae5ec9131020d4ce82ffaf
doihachiro <raiton20@gmail.com>
2023-01-09 14:20:07 +0900

    Add abc193_b

--- ABC/abc193/b/main.py
+++ ABC/abc193/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+N = int(input())
+ans = []
+
+for _ in range(N):
+    # A: minute, P: price, X: number
+    A, P, X = map(int, input().split())
+    if A + 0.5 < X:
+        ans.append(P)
+
+if len(ans) == 0:
+    print(-1)
+else:
+    print(min(ans))
