dbb4aefb4a1ddbc28f6b6692051cc49fdd9530c2
doihachiro <raiton20@gmail.com>
2023-01-09 14:20:07 +0900

    Add abc188_b

--- ABC/abc188/b/main.py
+++ ABC/abc188/b/main.py
@@ -1,1 +1,12 @@
-A, B = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+X = 0
+
+for i, j in zip(A, B):
+    X += i * j
+
+if X == 0:
+    print("Yes")
+else:
+    print("No")
