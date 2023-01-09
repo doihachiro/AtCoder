5232fcf8af884ec75299f1ab86e7104cbde8eead
doihachiro <raiton20@gmail.com>
2023-01-10 00:37:51 +0900

    Add abc194_b

--- ABC/abc194/b/main.py
+++ ABC/abc194/b/main.py
@@ -1,1 +1,20 @@
-A, B = map(int, input().split())
+N = int(input())
+ans = 10 ** 6
+A = []
+B = []
+
+
+for _ in range(N):
+    X, Y = map(int, input().split())
+    A.append(X)
+    B.append(Y)
+
+for i in range(N):
+    for j in range(N):
+        if i == j:
+            tmp = A[i] + B[j]
+        else:
+            tmp = max(A[i], B[j])
+        ans = min(ans, tmp)
+
+print(ans)
