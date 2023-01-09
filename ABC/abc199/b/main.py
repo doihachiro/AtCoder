792eb3b0b398e231076757712848702b6e53de76
doihachiro <raiton20@gmail.com>
2023-01-10 00:37:51 +0900

    Add abc199_b

--- ABC/abc199/b/main.py
+++ ABC/abc199/b/main.py
@@ -1,1 +1,8 @@
-A, B = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+MAX = max(A)
+MIN = min(B)
+
+print(max(0, MIN - MAX + 1))
