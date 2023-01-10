eb39e9b6b97cd5f1bc9f8adeaaee31b2f7fd88b8
doihachiro <raiton20@gmail.com>
2023-01-11 01:54:42 +0900

    Add abc201_b

--- ABC/abc201/b/main.py
+++ ABC/abc201/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N = int(input())
+tmp = []
+
+for _ in range(N):
+    S, T = input().split()
+    tmp.append([int(T), S])
+
+ans = sorted(tmp)
+
+print(ans[-2][1])
