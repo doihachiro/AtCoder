e2591d450016b971824aa0487d8bfba7bfa3df3e
doihachiro <raiton20@gmail.com>
2023-01-11 01:54:42 +0900

    Add abc202_b

--- ABC/abc202/b/main.py
+++ ABC/abc202/b/main.py
@@ -1,1 +1,12 @@
-A, B = map(int, input().split())
+S = input()
+X = []
+
+for i in S:
+    if i == "6":
+        X.append("9")
+    elif i == "9":
+        X.append("6")
+    else:
+        X.append(i)
+
+print("".join(reversed(X)))
