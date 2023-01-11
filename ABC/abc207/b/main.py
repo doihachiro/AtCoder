7a36cd2fb3b5881abf3f1a59dbae5e6cec5ac015
doihachiro <raiton20@gmail.com>
2023-01-12 01:30:08 +0900

    Add abc207_b

--- ABC/abc207/b/main.py
+++ ABC/abc207/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+A, B, C, D = map(int, input().split())
+R = 0
+count = 0
+
+if C * D <= B:
+    print(-1)
+else:
+    while R * D < A:
+        A += B
+        R += C
+        count += 1
+    else:
+        print(count)
