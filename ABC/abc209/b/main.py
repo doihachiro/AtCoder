bca973424cc5774c2a4f476f4bc18a7d6b873440
doihachiro <raiton20@gmail.com>
2023-01-12 01:30:08 +0900

    Add abc209_b

--- ABC/abc209/b/main.py
+++ ABC/abc209/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+tmp = 0
+
+for i in range(N):
+    if (i + 1) % 2 == 0:
+        A[i] -= 1
+    tmp += A[i]
+
+if tmp <= X:
+    print("Yes")
+else:
+    print("No")
