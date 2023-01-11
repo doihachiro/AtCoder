b55af4f65b1b2bf16b41d19392952b103997b7db
doihachiro <raiton20@gmail.com>
2023-01-12 01:30:08 +0900

    Add abc210_b

--- ABC/abc210/b/main.py
+++ ABC/abc210/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N = int(input())
+S = list(map(int, input()))
+
+for i in range(N):
+    if S[i] == 1:
+        if (i + 1) % 2 == 0:
+            print("Aoki")
+        else:
+            print("Takahashi")
+        exit()
