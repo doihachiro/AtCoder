36f1af294f2ccd9b945eff677fa3d1e74447a9be
doihachiro <raiton20@gmail.com>
2023-01-14 01:09:44 +0900

    Add abc216_b

--- ABC/abc216/b/main.py
+++ ABC/abc216/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N = int(input())
+ans = []
+
+for _ in range(N):
+    ans.append(tuple(input().split())) 
+
+if len(ans) != len(set(ans)):
+    print("Yes")
+else:
+    print("No")
