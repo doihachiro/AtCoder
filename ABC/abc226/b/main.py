08b510a6b465334a7e9c14b2b1b9553c8616c21d
doihachiro <raiton20@gmail.com>
2023-01-14 23:25:03 +0900

    Add abc226_b

--- ABC/abc226/b/main.py
+++ ABC/abc226/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+N = int(input())
+tmp = []
+
+for _ in range(N):
+    tmp.append(list(map(int, input().split())))
+
+for i in range(N):
+    tmp[i] = tuple(tmp[i])
+
+ans = tuple(tmp)
+print(len(set(ans)))
