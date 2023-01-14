a72689b8ebca176e04b669d95a3ed060bd56295a
doihachiro <raiton20@gmail.com>
2023-01-14 15:56:03 +0900

    Add abc221_b

--- ABC/abc221/b/main.py
+++ ABC/abc221/b/main.py
@@ -1,1 +1,17 @@
-A, B = map(int, input().split())
+S = list(input())
+V = list(input())
+N = len(S)
+
+if S == V:
+    print("Yes")
+    exit()
+
+for i in range(N-1):
+    S[i], S[i+1] = S[i+1], S[i]
+    if S == V:
+        print("Yes")
+        exit()
+    else:
+        S[i], S[i+1] = S[i+1], S[i]
+
+print("No")
