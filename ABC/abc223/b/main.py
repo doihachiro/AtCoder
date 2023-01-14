b64280bc047e89185cf60387b3efa634d2aee944
doihachiro <raiton20@gmail.com>
2023-01-14 15:56:03 +0900

    Add abc223_b

--- ABC/abc223/b/main.py
+++ ABC/abc223/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+S = list(input())
+ans = []
+
+for i in range(len(S)):
+    tmp = S[0]
+    del S[0]
+    S.append(tmp)
+    ans.append("".join(S))
+
+ans.sort()
+
+print(ans[0])
+print(ans[-1])
