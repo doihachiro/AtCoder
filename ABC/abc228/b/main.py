614f0e027e160acc8199e2f456e1b6e2e3be59cf
doihachiro <raiton20@gmail.com>
2023-01-14 23:25:14 +0900

    Add abc228_b

--- ABC/abc228/b/main.py
+++ ABC/abc228/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+count = [0] * N
+
+while True:
+    if count[X-1] == 1:
+        print(sum(count))
+        exit()
+    count[X-1] = 1
+    X = A[X-1]
