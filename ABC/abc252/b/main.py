9fcc33c50935c7efe73c6506a95f736e4794238b
doihachiro <raiton20@gmail.com>
2023-01-15 16:10:59 +0900

    Add abc252_b

--- ABC/abc252/b/main.py
+++ ABC/abc252/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+tmp = [False] * N
+m = max(A)
+
+for i in range(N):
+    if A[i] == m and i+1 in B:
+        print("Yes")
+        exit()
+
+print("No")
