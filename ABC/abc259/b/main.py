2f897414f7bc081a5a27d877518b39f78aab43ff
doihachiro <raiton20@gmail.com>
2023-01-15 22:30:18 +0900

    Add abc259_b

--- ABC/abc259/b/main.py
+++ ABC/abc259/b/main.py
@@ -1,1 +1,8 @@
-A, B = map(int, input().split())
+import math
+a, b, d = map(int, input().split())
+rad_d = math.radians(d)
+
+ans_a = a * math.cos(rad_d) - b * math.sin(rad_d)
+ans_b = a * math.sin(rad_d) + b * math.cos(rad_d)
+
+print(ans_a, ans_b)
