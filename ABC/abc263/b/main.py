01a2b3032691d9d6bc8e66ef52a825bd65111523
doihachiro <raiton20@gmail.com>
2023-01-15 22:30:18 +0900

    Add abc263_b

--- ABC/abc263/b/main.py
+++ ABC/abc263/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N = int(input())
+P = [0] + [0] + list(map(int, input().split()))
+i = N
+count = 1
+
+while P[i] != 1:
+    count += 1
+    i = P[i]
+
+print(count)
