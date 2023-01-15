5e0675b806be1eac1f82c78392dbb963d4338d10
doihachiro <raiton20@gmail.com>
2023-01-15 16:10:59 +0900

    Add abc254_b

--- ABC/abc254/b/main.py
+++ ABC/abc254/b/main.py
@@ -1,1 +1,12 @@
-A, B = map(int, input().split())
+N = int(input())
+tmp = [[] for _ in range(N)]
+
+for i in range(N):
+    for j in range(i+1):
+        if j == 0 or i == j:
+            tmp[i].append(1)
+        else:
+            tmp[i].append(tmp[i-1][j-1] + tmp[i-1][j])
+
+for k in tmp:
+    print(*k)
