217ab9dfc3e601f3c5eb0eda4db17d34a7845801
doihachiro <raiton20@gmail.com>
2023-01-15 16:10:59 +0900

    Add abc256_b

--- ABC/abc256/b/main.py
+++ ABC/abc256/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
+
+S = sum(A)
+P = 0
+
+for i in range(N):
+    if S >= 4:
+        P += 1
+        S -= A[i]
+print(P)
