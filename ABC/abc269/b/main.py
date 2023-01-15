5cd42ad4f2bdfbfb6b0e09e71c4aacbda86f5c70
doihachiro <raiton20@gmail.com>
2023-01-16 01:06:50 +0900

    Add abc269_b

--- ABC/abc269/b/main.py
+++ ABC/abc269/b/main.py
@@ -1,1 +1,13 @@
-A, B = map(int, input().split())
+AB = []
+CD = []
+
+for i in range(1, 11):
+    tmp = list(input())
+    if "#" in tmp:
+        AB.append(i)
+        for j in range(10):
+            if tmp[j] == "#":
+                CD.append(j+1)
+
+print(min(AB), max(AB))
+print(min(CD), max(CD))
