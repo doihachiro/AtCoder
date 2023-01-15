32b8db6e3c0c816a6da9bc4564373b8e329f91d1
doihachiro <raiton20@gmail.com>
2023-01-15 16:10:59 +0900

    Add abc257_b

--- ABC/abc257/b/main.py
+++ ABC/abc257/b/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N, K, Q = map(int,input().split())
+A = list(map(int,input().split())) + [N+1]
+L = list(map(int,input().split()))
+
+for l in L:
+  l -= 1
+  if A[l+1] - A[l] != 1:
+    A[l] += 1
+    
+print(*A[:-1])
