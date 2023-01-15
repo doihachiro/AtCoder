651a1a6dd12004b40fbb1eaaef0a17c5fe6c200d
doihachiro <raiton20@gmail.com>
2023-01-16 01:06:50 +0900

    Add abc268_b

--- ABC/abc268/b/main.py
+++ ABC/abc268/b/main.py
@@ -1,1 +1,14 @@
-A, B = map(int, input().split())
+S = input()
+T = input()
+
+
+if len(T) < len(S):
+    print("No")
+    exit()
+else:
+    for i in range(len(S)):
+        if S[i] != T[i]:
+            print("No")
+            exit()
+            
+print("Yes")
