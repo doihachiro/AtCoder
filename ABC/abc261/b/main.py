6a77ac9cd7f66603511678fe7042ab7f652c0d0b
doihachiro <raiton20@gmail.com>
2023-01-15 22:30:18 +0900

    Add abc261_b

--- ABC/abc261/b/main.py
+++ ABC/abc261/b/main.py
@@ -1,1 +1,11 @@
-A, B = map(int, input().split())
+N = int(input())
+A = [list(map(str, input())) for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if i != j:
+            if (A[i][j] == "W" and A[j][i] != "L") or (A[i][j] == "L" and A[j][i] != "W") or (A[i][j] == "D" and A[j][i] != "D"):
+                print("incorrect")
+                exit()
+
+print("correct")
