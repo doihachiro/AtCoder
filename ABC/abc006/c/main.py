97539a6f545571b4b395bbb1ed5cfa0ee5518c5d
doihachiro <raiton20@gmail.com>
2023-01-23 21:44:35 +0900

    Add abc006_c

--- ABC/abc006/c/main.py
+++ ABC/abc006/c/main.py
@@ -1,1 +1,10 @@
-A, B = map(int, input().split())
+N, M = map(int, input().split())
+
+for i in range(N+1):
+    j = 4*N - M - 2*i
+    k = M + i - 3*N
+    if 0 <= j and 0 <= k:
+        print(i, j, k)
+        exit()
+
+print(-1, -1, -1)
