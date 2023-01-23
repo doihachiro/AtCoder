8d64199c13293ec388059db3ffca9bea0c9f4ee3
doihachiro <raiton20@gmail.com>
2023-01-23 21:44:35 +0900

    Add abc003_c

--- ABC/abc003/c/main.py
+++ ABC/abc003/c/main.py
@@ -1,1 +1,9 @@
-A, B = map(int, input().split())
+N, K = map(int, input().split())
+R = list(map(int, input().split()))
+ans = 0
+
+R.sort()
+for i in range(N-K, N):
+    ans = (ans + R[i]) / 2
+
+print(ans)
