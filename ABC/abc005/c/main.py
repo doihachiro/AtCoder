205d6c1a9052064ead87909e64607a6243345f1b
doihachiro <raiton20@gmail.com>
2023-01-23 21:44:35 +0900

    Add abc005_c

--- ABC/abc005/c/main.py
+++ ABC/abc005/c/main.py
@@ -1,1 +1,18 @@
-A, B = map(int, input().split())
+from collections import deque
+
+T = int(input())
+N = int(input())
+A = deque(map(int, input().split()))
+M = int(input())
+B = list(map(int, input().split()))
+
+for i in B:
+    while True:
+        if len(A) == 0:
+            print("no")
+            exit()
+        j = A.popleft()
+        if j <= i <= j + T:
+            break
+
+print("yes")
